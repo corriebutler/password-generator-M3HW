fd9756a7bc3b89e23b46e98cd6c14577a108bbed
Corrie Butler <corriebutler12@gmail.com>
2021-09-24 11:08:29 -0600

    prompt for character count and types.

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  prompt("How many characters would you like your password to contain?");
+  confirm("Click OK to confirm including special characters.");
+  confirm("Click OK to confirm including numeric characters.");
+  confirm("Click OK to confirm including lowercase characters.");
+  confirm("Click OK to confirm including uppercase characters."); 
+
   passwordText.value = password;
 
 }
